4d93477a5db5a2007e73367499d5da356a3d15e6
Vinicius Benedito <beneditoloura@gmail.com>
2020-09-29 13:06:58 -0300

    Clonepipefy finalizado

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,7 +4,7 @@ import Card from '../Card';
 import more from '../../assets/more.png';
 import { Container } from './styles';
 
-function List({ data }) {
+function List({ data, index: listIndex }) {
     return (
         <Container done={data.done}>
             <header>
@@ -17,7 +17,7 @@ function List({ data }) {
             </header>
 
             <ul>
-                {data.cards.map(card => <Card key={card.id} data={card} />)}
+                {data.cards.map((card, index) => <Card key={card.id} index={index} listIndex={listIndex} data={card} />)}
             </ul>
         </Container>
     );
